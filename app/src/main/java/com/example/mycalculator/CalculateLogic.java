94e127232dbabdbb43e3e1b706271abb72faa45c
YuryYakovlev <yakor_101@mail.ru>
2021-09-10 20:00:09 +1000

    Calculate logic

---
+++ app/src/main/java/com/example/mycalculator/CalculateLogic.java
@@ -0,0 +1,136 @@
+package com.example.mycalculator;
+
+public class CalculateLogic {
+
+    StringBuilder stringBuilder = new StringBuilder();
+
+    private int firstArg;
+    private int secondArg;
+    private char symbolOperation;
+    private State state;
+
+    public CalculateLogic() {
+        state = State.FIRST_NUMBER;
+    }
+
+    private enum State {
+        FIRST_NUMBER,
+        OPERATION,
+        SECOND_NUMBER,
+        EQUALS
+    }
+
+    public void numberClick(int buttonId) {
+        if (stringBuilder.length() < 9) {
+
+            if (state == State.EQUALS) {
+                stringBuilder.setLength(0);
+                state = State.FIRST_NUMBER;
+            }
+            if (state == State.OPERATION) {
+                stringBuilder.setLength(0);
+                state = State.SECOND_NUMBER;
+            }
+            switch (buttonId) {
+                case R.id.btn_one:
+                    stringBuilder.append("1");
+                    break;
+                case R.id.btn_two:
+                    stringBuilder.append("2");
+                    break;
+                case R.id.btn_three:
+                    stringBuilder.append("3");
+                    break;
+                case R.id.btn_four:
+                    stringBuilder.append("4");
+                    break;
+                case R.id.btn_five:
+                    stringBuilder.append("5");
+                    break;
+                case R.id.btn_six:
+                    stringBuilder.append("6");
+                    break;
+                case R.id.btn_seven:
+                    stringBuilder.append("7");
+                    break;
+                case R.id.btn_eight:
+                    stringBuilder.append("8");
+                    break;
+                case R.id.btn_nine:
+                    stringBuilder.append("9");
+                    break;
+                case R.id.btn_zero:
+                    if (stringBuilder.length() != 0) {
+                        stringBuilder.append("0");
+                    }
+                    return;
+            }
+            if (state == State.FIRST_NUMBER) {
+                firstArg = Integer.parseInt(stringBuilder.toString());
+            } else if (state == State.SECOND_NUMBER) {
+                secondArg = Integer.parseInt(stringBuilder.toString());
+            }
+        }
+    }
+
+    public void operationClick(int buttonId) {
+        if (state == State.FIRST_NUMBER) {
+            state = State.OPERATION;
+            switch (buttonId) {
+                case R.id.btn_plus:
+                    symbolOperation = '+';
+                    break;
+                case R.id.btn_minus:
+                    symbolOperation = '-';
+                    break;
+                case R.id.btn_multiplication:
+                    symbolOperation = '*';
+                    break;
+                case R.id.btn_division:
+                    symbolOperation = '/';
+                    break;
+            }
+        }
+        if(state == State.SECOND_NUMBER) {
+            switch (buttonId) {
+                case R.id.btn_equals:
+                    equals(firstArg, secondArg, symbolOperation);
+                    break;
+            }
+        }
+    }
+
+    public void equals(int agrOne, int argTwo, char symbolOperation) {
+        if (firstArg != 0 && symbolOperation != ' ') {
+            state = State.EQUALS;
+            stringBuilder.setLength(0);
+            if (symbolOperation == '+') {
+                firstArg = agrOne + argTwo;
+            } else if (symbolOperation == '-') {
+                firstArg = agrOne - argTwo;
+            } else if (symbolOperation == '*') {
+                firstArg = agrOne * argTwo;
+            } else if (symbolOperation == '/') {
+                firstArg = agrOne / argTwo;
+            }
+        }
+    }
+
+    public void discharge() {
+        stringBuilder.setLength(0);
+        state = State.FIRST_NUMBER;
+    }
+
+    public String getText() {
+        switch (state) {
+            default:
+                return stringBuilder.toString();
+            case OPERATION:
+                stringBuilder.setLength(0);
+                return stringBuilder.append(symbolOperation).toString();
+            case EQUALS:
+                stringBuilder.setLength(0);
+                return stringBuilder.append(firstArg).toString();
+        }
+    }
+}
